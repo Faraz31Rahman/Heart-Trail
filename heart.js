bc9404557279705a0b6d29c819bfe6619db8c6d5
Faraz31Rahman <151454740+Faraz31Rahman@users.noreply.github.com>
2024-06-16 20:51:30 +0600

    Update heart.js

--- heart.js
+++ heart.js
@@ -17,3 +17,21 @@ bodyEl, addEventListener("mousemove", (event) => {
     spanEl.remove();
   }, 3000);
 })
+
+bodyEl, addEventListener("click", (event) => {
+  const xPos = event.offsetX;
+  const yPos = event.offsetY;
+  const spanEl = document.createElement("span");
+  spanEl.style.left = xPos + "px";
+  spanEl.style.top = yPos + "px";
+
+  let size = Math.random() * 100;
+  spanEl.style.width = size + "px";
+  spanEl.style.height = size + "px";
+
+
+  bodyEl.appendChild(spanEl);
+  setTimeout(() => {
+    spanEl.remove();
+  }, 3000);
+})
